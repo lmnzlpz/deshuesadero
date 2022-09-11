854e5d2fc645f6d2493f079867ba056c42bd7809
leonardomunoz <l.mnz.lpz@gmail.com>
2022-09-10 22:43:28 -0600

    only static hosting

--- dbConnector.js
+++
@@ -1,8 +0,0 @@
-const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = "mongodb+srv://leor4p44:adAstra123@cluster0.zakpgia.mongodb.net/?retryWrites=true&w=majority";
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-  const collection = client.db("test").collection("devices");
-  // perform actions on the collection object
-  client.close();
-});
